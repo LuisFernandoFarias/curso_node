9eb2d72ecfd77b242c799327e377f4c0451b9dbb
Luis <luis.fernando@acct.global>
2023-08-03 17:50:24 -0300

    fix: commit

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
 app.get('/user/:nome/sobrenome/:sobrenome', function (req, res) {
   res.send({
     nome: req.params.nome,
-    sobrenome: req.params.sobrenome
+    sobrenome: req.params.sobrenome,
   })
 })
 
