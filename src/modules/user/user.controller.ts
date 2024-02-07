8bd5944aa7f70d2e5edb70ca70155ae0dd13355c
Luís Fernando <lu.is.fernando@hotmail.com>
2024-02-07 08:46:01 -0300

    feat: find user by token

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -7,6 +7,7 @@ import { ReturnError } from '@exceptions/dtos/return-error.dtos'
 import { authMiddleware } from 'src/middlewares/auth.middleware'
 import { authAdminMiddleware } from 'src/middlewares/auth-admin.middlewarw'
 import { UserEditPasswordDTO } from './dtos/user-edit-password.dto'
+import { getUserByToken } from '@utils/auth'
 
 const createUserController = async (req: Request<core.ParamsDictionary, any, UserInsertDTO>, res: Response): Promise<void> => {
   const user = await createUser(req.body).catch((error) => {
@@ -27,7 +28,8 @@ const getUsersController = async (req: Request, res: Response): Promise<void> =>
 }
 
 const editPasswordController = async (req: Request<undefined, undefined, UserEditPasswordDTO>, res: Response): Promise<void> => {
-  const user = await editPassword(15, req.body).catch((error) => {
+  const userAuth = await getUserByToken(req)
+  const user = await editPassword(userAuth.userId, req.body).catch((error) => {
     new ReturnError(res, error)
   })
   res.send(user)
